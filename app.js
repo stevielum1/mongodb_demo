358b0a2351f8ddcb0b458b3049f1431b1dfaae19
Steven Lum <stevielum1@gmail.com>
2018-08-25 23:01:19 -0700

    Add User and Event models

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
+const events = require('./routes/api/events');
 
 mongoose
   .connect(db)
-  .then(() => console.log("Successfully connected to MongoDB"))
+  .then(() => console.log('Successfully connected to MongoDB'))
   .catch(err => console.log(err));
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello world");
+  res.send('Hello world');
 });
+
+app.use('/api/users', users);
+app.use('/api/events', events);
+
+// Middleware for body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
